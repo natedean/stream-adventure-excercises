d6d9cf0332facbace2dc9f2cd49ad2ebca0e5f80
Nate Dean <natedeanmail@gmail.com>
2016-01-10 16:52:04 -0600

    Excercise 7 complete

--- 7_sa.js
+++ 7_sa.js
@@ -1,8 +1,24 @@
 const http = require('http');
 const fs = require('fs');
+const through = require('through2');
+const PORT = process.argv[2];
+const stream = through(write, end);
+
 
 const server = http.createServer((req, res)=>{
-  fs.createReadStream('data.txt').pipe(res);
+  if(req.method === 'POST'){
+    req.pipe(stream).pipe(res);
+  }
+
 });
 
-server.listen(3000);
+server.listen(PORT);
+
+function write(buffer, encoding, next){
+  this.push(buffer.toString().toUpperCase());
+  next();
+}
+
+function end(done){
+  done();
+}
